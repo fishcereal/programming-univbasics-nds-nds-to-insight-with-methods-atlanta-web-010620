31c0062c27c63804a48737e21aa3d513dac4b850
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-06 16:12:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,8 +40,8 @@ def gross_for_director(director_data)
     total += director_data[:movies][counter][:worldwide_gross]
     counter +=1 
   end
-  result.store(director_data[:name],total)
-  return total 
+  result.store(total)
+  pp  result  
 
   # pp director_data[:movies][0][:worldwide_gross]
 end
