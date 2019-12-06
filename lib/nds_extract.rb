eeaa9e8043508e6914a3e74803c3682792853a24
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-06 15:55:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,6 +8,20 @@ require 'directors_database'
 def directors_totals(nds)
   result = {}
   nil
+  
+# start outside loop 
+
+outside_counter = 0
+while outside_counter < directors_database.length do 
+    total = 0 
+    counter = 0 
+    while counter < directors_database[outside_counter][:movies].length do 
+        total += directors_database[outside_counter][:movies][counter][:worldwide_gross]
+        counter+=1
+    end
+    result.store(directors_database[outside_counter][:name],total)
+    outside_counter+=1
+end
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
